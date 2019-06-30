70295eb1b6388cc8ab0afd0cd1833039cea64a1a
Sanjay Suresh/JAM/Brenntag <sanjaysuresh15@gmail.com>
2019-06-29 19:47:08 -0500

    no change actually making the change

--- pybank/main.py
+++ pybank/main.py
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
 import os
 import csv
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 with open(csvpath, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
     
-    dates = []
-    winsorloss = []
-    for row in csvreader:
-        date = row[0]
-        winorloss = row[1]
-
-        dates.append(date)
-        winsorloss.append(winorloss)
-
-    print(date)
-    print(winorloss)
 
 
 
-=======
->>>>>>> a4f3069a0a8dd87488f330c5d2af775237b43968
+
+
+
 
